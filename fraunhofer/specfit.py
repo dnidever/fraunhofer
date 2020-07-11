bfbb532f6142e0d4fa3da424a90dd2cd19faff62
David Nidever <dnidever@lsst.org>
2020-07-11 13:17:23 -0600

    Adding initial files

---
+++ fraunhofer/specfit.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""SPECFIT.PY - Generic stellar abundance determination software
+
+"""
+
+from __future__ import print_function
+
+__authors__ = 'David Nidever <dnidever@montana.edu>'
+__version__ = '20200711'  # yyyymmdd
+
+import os
+#import sys, traceback
+import contextlib, io, sys
+import numpy as np
+import warnings
+from astropy.io import fits
+from astropy.table import Table
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.wcs import WCS
+from scipy.ndimage.filters import median_filter,gaussian_filter1d
+from scipy.optimize import curve_fit, least_squares
+from scipy.interpolate import interp1d
+import thecannon as tc
+from dlnpyutils import utils as dln, bindata
+from .spec1d import Spec1D
+from . import (cannon,utils,reader)
+import copy
+import emcee
+import corner
+import logging
+import time
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.legend import Legend
+
+# Ignore these warnings, it's a bug
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+
+cspeed = 2.99792458e5  # speed of light in km/s
+
+def fit(name):
+    """ Fit a spectrum and determine the abundances."""
+    pass
