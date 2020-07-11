a91a833810bb21227b4bea73c3cbaf8d65e5133c
David Nidever <dnidever@lsst.org>
2020-07-11 13:17:35 -0600

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='Fraunhofer',
+      version='1.0',
+      description='Generic Steller Abundance Determination Software',
+      author='David Nidever',
+      author_email='dnidever@montana.edu',
+      url='https://github.com/dnidever/fraunhofer',
+      packages=find_packages(exclude=["tests"]),
+      scripts=['bin/fraunhofer'],
+      requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils','doppler'],
+      #include_package_data=True,
+)
