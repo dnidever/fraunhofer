cf346ba87f1b6d6a5dc5c479ccfe4a411d890637
David Nidever <dnidever@lsst.org>
2021-02-01 13:07:07 -0700

    Updates to models.py

--- fraunhofer/models.py
+++ fraunhofer/models.py
@@ -22,7 +22,7 @@ from astropy.coordinates import SkyCoord, EarthLocation
 from astropy.wcs import WCS
 from scipy.ndimage.filters import median_filter,gaussian_filter1d
 from scipy.optimize import curve_fit, least_squares
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, interpn
 import thecannon as tc
 from dlnpyutils import utils as dln, bindata
 from doppler.spec1d import Spec1D
@@ -50,6 +50,34 @@ kpath = '/Users/nidever/synspec/winter2017/odfnew/'
 # mkmod.pro  make model using interpolation
 # mkmadaf.pro   create a composition/continuum opacity file
 
+def trapz(x,y):
+    """
+       Numerical integration using the composed trapezoidal rule
+
+       int[f(x)dx] ~= 1/2 (y1*(X2-X1)+yn*(Xn-Xn-1))+ 1/2*sum_i[yi*(Xi+1-Xi-1)]
+
+       IN: x   - fltarr        abscisas
+           y   - fltarr        ordinates
+
+       OUT: trazp - float      the numerical approx. to the integral of y(x)
+       
+       NOTE: double precision is used
+
+       C. Allende Prieto, Sep 1998
+       ", March 2010, changed loop variable to long
+    """
+    
+    n = len(x)-1
+    trapz = 0.5*y[0]*(x[1]-x[0])
+    for i in np.arange(n-1)+1:
+        trapz = trapz+0.5*y[i]*(x[i+1]-x[i-1])
+
+    if (n > 0):
+        trapz = trapz+0.5*y[n]*(x[n]-x[n-1])
+
+    return trapz
+
+
 def read_kurucz(teff,logg,metal,mtype='odfnew'):
     """ Read a Kurucz model from the large grid."""
     #kpath = 'odfnew/'
@@ -76,9 +104,9 @@ def read_kurucz(teff,logg,metal,mtype='odfnew'):
 
     with open(filename,'r') as fil:
         line = fil.readline()
-        while (line != '') and (line.find(teffstring) == -1) and (line.find(loggstring) == -1):
+        while (line != '') and ((line.find(teffstring) == -1) or (line.find(loggstring) == -1)):
             line = fil.readline()
-
+            
         while (line.find('READ') == -1):
             header.append(line.rstrip())
             line = fil.readline()
@@ -108,7 +136,7 @@ def read_kurucz(teff,logg,metal,mtype='odfnew'):
 
 
 
-def mkmod(teff,logg,metal,outfile,ntau=None,mtype='odfnew'):
+def mkmod(teff,logg,metal,outfile=None,ntau=None,mtype='odfnew'):
     """
 	Extracts and if necessary interpolates (linearly) a kurucz model 
 	from his grid.
@@ -175,8 +203,10 @@ def mkmod(teff,logg,metal,outfile,ntau=None,mtype='odfnew'):
         v2,nv2 = dln.where(abs(availlogg-logg) <= 0.001)
         v3,nv3 = dln.where(abs(availmetal-metal) <= 0.001)
 
-        if (teff <= max(availteff) and teff >= min(availteff) and logg <= max(availlogg) and logg <= min(availlogg) and metal >= min(availmetal) and metal <= max(availmetal)):
-            
+        if (teff <= max(availteff) and teff >= min(availteff) and logg <= max(availlogg) and
+            logg >= min(availlogg) and metal >= min(availmetal) and metal <= max(availmetal)):
+
+            # Model found, just read it
             if (nv1>0 and nv2>0 and nv3>0):
                 # Direct extraction of the model
                 teff = availteff[v1[0]]
@@ -184,49 +214,73 @@ def mkmod(teff,logg,metal,outfile,ntau=None,mtype='odfnew'):
                 metal = availmetal[v3[0]]
                 model,header,tail = read_kurucz(teff,logg,metal,mtype=mtype)
                 ntau = len(model[0,:])
-	
+            # Need to interpolate
             else:
-                import pdb; pdb.set_trace()
-                model = model_interp(teff,logg,metal)
-
-                
+                model,header,tail = model_interp(teff,logg,metal,mtype=mtype)
 
         else:
             print('% KMOD:  The requested values of ([Fe/H],logg,Teff) fall outside')
             print('% KMOD:  the boundaries of the grid.')
             print('% KMOD:  Temperatures higher that 10000 K can be reached, by modifying rd_kmod.')
 
+    import pdb; pdb.set_trace()
 
                 
-    ## writing the outputfile
-    #openw,u,outfile
-    #for i in range(len(header)):
-    #    printf,u,header(i)
-    #if type == 'old':
-    #    for i in range(ntau):
-    #        print(model[:,i],format='(E15.8,x,f8.1,5(x,E9.3))')
-    #else:
-    #    for i in range(ntau):
-    #        print(model[:,i],format='(E15.8,x,f8.1,8(x,E9.3))')
-
-    #for i in range(len(tail)):
-    #    printf,u,tail(i)
+    # Writing the outputfile
+    if outfile is not None:
+        with open(outfile,'w') as fil:
+            for i in range(len(header)):
+                fil.write(header[i]+'\n')
+        if type == 'old':
+            for i in range(ntau):
+                fil.write(model[:,i],format='(E15.8,x,f8.1,5(x,E9.3))')
+        else:
+            for i in range(ntau):
+                fil.write(model[:,i],format='(E15.8,x,f8.1,8(x,E9.3))')
+        for i in range(len(tail)):
+            fil.write(tail[i]+'\n')
 
     return model, header, tail
 
 
-                
-                
 
-def model_interp(teff,logg,metal):
-                
+def model_interp(teff,logg,metal,mtype='odfnew'):
+    """ Interpolate Kurucz model."""
+    
+    availteff = np.arange(27)*250+3500.0
+    availlogg = np.arange(11)*.5+0.
+    availmetal = np.arange(7)*0.5-2.5
+
+    if mtype is None:
+        mtype='odfnew'
+    if mtype == 'old':
+        availmetal = np.arange(13)*0.5-5.0
+
+    if mtype == 'old':
+        ntau = 64
+    else:
+        ntau = 72
+
+    if mtype == 'odfnew' and teff > 10000:
+        avail = Table.read(kpath+'tefflogg.txt',format='ascii')
+        avail['col1'].name = 'teff'
+        avail['col2'].name = 'logg'
+        v1,nv1 = dln.where(abs(avail['teff']-teff) < 0.1 and abs(avail['logg']-logg) <= 0.001)
+        v2 = v1
+        v3,nv3 = dln.where(abs(availmetal-metal) <= 0.001)
+    else:
+        v1,nv1 = dln.where(abs(availteff-teff) <= .1)
+        v2,nv2 = dln.where(abs(availlogg-logg) <= 0.001)
+        v3,nv3 = dln.where(abs(availmetal-metal) <= 0.001)
+
+    
     # Linear Interpolation 
-    teffimif = max(np.where(availteff <= teff))     # immediately inferior Teff
-    loggimif = max(np.where(availlogg <= logg))     # immediately inferior logg
-    metalimif = max(np.where(availmetal <= metal))  #immediately inferior [Fe/H]
-    teffimsu = min(np.where(availteff >= teff))     # immediately superior Teff
-    loggimsu = min(np.where(availlogg >= logg))     # immediately superior logg
-    metalimsu = min(np.where(availmetal >= metal))  #immediately superior [Fe/H]
+    teffimif = max(np.where(availteff <= teff)[0])     # immediately inferior Teff
+    loggimif = max(np.where(availlogg <= logg)[0])     # immediately inferior logg
+    metalimif = max(np.where(availmetal <= metal)[0])  #immediately inferior [Fe/H]
+    teffimsu = min(np.where(availteff >= teff)[0])     # immediately superior Teff
+    loggimsu = min(np.where(availlogg >= logg)[0])     # immediately superior logg
+    metalimsu = min(np.where(availmetal >= metal)[0])  #immediately superior [Fe/H]
 	
     if mtype == 'old':
         ncols = 7
@@ -253,10 +307,9 @@ def model_interp(teff,logg,metal):
         mapmetal = (metal-mm1)/(mp1-mm1)
     else:
         mapmetal = 0.5
-
-    # Reading the corresponding models
-    
-    for i in range(8):
+        
+    # Reading the corresponding models    
+    for i in np.arange(8)+1:
         if i == 1: model,header,tail = read_kurucz(tm1,lm1,mm1,mtype=mtype)
         if i == 2: model,h,t = read_kurucz(tm1,lm1,mp1,mtype=mtype)
         if i == 3: model,h,t = read_kurucz(tm1,lp1,mm1,mtype=mtype)
@@ -265,104 +318,108 @@ def model_interp(teff,logg,metal):
         if i == 6: model,h,t = read_kurucz(tp1,lm1,mp1,mtype=mtype)
         if i == 7: model,h,t = read_kurucz(tp1,lp1,mm1,mtype=mtype)
         if i == 8: model,h,t = read_kurucz(tp1,lp1,mp1,mtype=mtype)
-
+        
         if (len(model[0,:]) > ntau):
             m2 = np.zeros((ncols,ntau),dtype=np.float64)
             m2[0,:] = interpol(model[0,:],ntau)
             for j in range(ncols):
                 m2[j,:] = interpol(model[j,:],model[0,:],m2[0,:])
             model = m2
-	    # getting the tauross scale
-            rhox = model[0,:]
-            kappaross = model[4,:]
-            tauross = np.zeros(ntau,dtype=np.float64)
-            tauross[0] = rhox[0]*kappaross[0]
-            for ii in np.arange(ntau-1)+1:
-                tauross[ii] = trapz(rhox[0:ii],kappaross[0:ii])
+            
+	# getting the tauross scale
+        rhox = model[0,:]
+        kappaross = model[4,:]
+        tauross = np.zeros(ntau,dtype=np.float64)
+        tauross[0] = rhox[0]*kappaross[0]
+        for ii in np.arange(ntau-1)+1:
+            tauross[ii] = trapz(rhox[0:ii+1],kappaross[0:ii+1])
 
-            if case==1:
-                model1 = model 
-                tauross1 = tauross
-            elif case==2:
-                model2 = model
-                tauross2 = tauross
-            elif case==3:
-                model3 = model 
-                tauross3 = tauross
-            elif case==4:
-                model4 = model 
-                tauross4 = tauross
-            elif case==5:
-                model5 = model 
-                tauross5 = tauross
-            elif case==6:
-                model6 = model 
-                tauross6 = tauross
-            elif case==7:
-                model7 = model 
-                tauross7 = tauross
-            elif case==8:
-                model8 = model 
-                tauross8 = tauross
-            else:
-                print('% KMOD: i should be 1--8!')
-
-        model = np.zeros((ncols,ntau),dtype=np.float64)  # cleaning up for re-using the matrix
-
-        # defining the  mass (RHOX#gr cm-2) sampling 
-        tauross = tauross1       # re-using the vector tauross
-        bot_tauross = min([tauross1[ntau-1],tauross2[ntau-1],
-                           tauross3[ntau-1],tauross4[ntau-1],
-                           tauross5[ntau-1],tauross6[ntau-1],
-                           tauross7[ntau-1],tauross8[ntau-1]])
-        top_tauross = max([tauross1[0],tauross2[0],tauross3[0],
-                           tauross4[0],tauross5[0],tauross6[0],
-                           tauross7[0],tauross8[0]])
-        g = np.where(tauross >= top_tauross and tauross <= bot_tauross)
-        ntau = len(g)
-        tauross_new = interpol(tauross[g])
-
-        # let's interpolate for every depth
-        for i in range(ntau):
-            for j in range(ncols):
-                grid[0,0,0,j] = interpol(model1[j,1:ntau-1],tauross1[1:ntau-1],tauross_new[i])
-                grid[0,0,1,j] = interpol(model2[j,1:ntau-1],tauross2[1:ntau-1],tauross_new[i])
-                grid[0,1,0,j] = interpol(model3[j,1:ntau-1],tauross3[1:ntau-1],tauross_new[i])
-                grid[0,1,1,j] = interpol(model4[j,1:ntau-1],tauross4[1:ntau-1],tauross_new[i])
-                grid[1,0,0,j] = interpol(model5[j,1:ntau-1],tauross5[1:ntau-1],tauross_new[i])
-                grid[1,0,1,j] = interpol(model6[j,1:ntau-1],tauross6[1:ntau-1],tauross_new[i])
-                grid[1,1,0,j] = interpol(model7[j,1:ntau-1],tauross7[1:ntau-1],tauross_new[i])
-                grid[1,1,1,j] = interpol(model8[j,1:ntau-1],tauross8[1:ntau-1],tauross_new[i])
-                model[j,i] = interpolate(grid[:,:,:,j],mapteff,maplogg,mapmetal)
-
-                for j in range(ncols):
-                    model[j,0] = model[j,1]*0.999
-
-
-        # editing the header
-        tmpstr = header[0]
-        strput,tmpstr,string(teff,format='(f7.0)'),5
-        strput,tmpstr,string(logg,format='(f8.5)'),21
-        header[0] = tmpstr
-        tmpstr1 = header[1]
-        tmpstr2 = header[4]
-        if (metal < 0.0):
-            if type == 'old':
-                strput,tmpstr1,string(abs(metal),format='("-",f3.1)'),18
-            else:
-                strput,tmpstr1,string(abs(metal),format='("-",f3.1)'),8
-            strput,tmpstr2,string(10^metal,format='(f9.5)'),16
+        if i==1:
+            model1 = model 
+            tauross1 = tauross
+        elif i==2:
+            model2 = model
+            tauross2 = tauross
+        elif i==3:
+            model3 = model 
+            tauross3 = tauross
+        elif i==4:
+            model4 = model 
+            tauross4 = tauross
+        elif i==5:
+            model5 = model 
+            tauross5 = tauross
+        elif i==6:
+            model6 = model 
+            tauross6 = tauross
+        elif i==7:
+            model7 = model 
+            tauross7 = tauross
+        elif i==8:
+            model8 = model 
+            tauross8 = tauross
         else:
-            if type == 'old':
-                strput,tmpstr1,string(abs(metal),format='("+",f3.1)'),18
-            else:
-                strput,tmpstr1,string(abs(metal),format='("+",f3.1)'),8
-            strput,tmpstr2,string(10^metal,format='(f9.5)'),16
-        header[1] = tmpstr1
-        header[4] = tmpstr2
-        tmpstr = header[22]
-        strput,tmpstr,string(ntau,format='(i2)'),11
-        header[22] = tmpstr
+            print('% KMOD: i should be 1--8!')
+
+    model = np.zeros((ncols,ntau),dtype=np.float64)  # cleaning up for re-using the matrix
+
+    # Defining the mass (RHOX#gr cm-2) sampling 
+    tauross = tauross1       # re-using the vector tauross
+    bot_tauross = min([tauross1[ntau-1],tauross2[ntau-1],
+                       tauross3[ntau-1],tauross4[ntau-1],
+                       tauross5[ntau-1],tauross6[ntau-1],
+                       tauross7[ntau-1],tauross8[ntau-1]])
+    top_tauross = max([tauross1[0],tauross2[0],tauross3[0],
+                       tauross4[0],tauross5[0],tauross6[0],
+                       tauross7[0],tauross8[0]])
+    g, = np.where((tauross >= top_tauross) & (tauross <= bot_tauross))
+    tauross_new = dln.interp(np.linspace(0,1,len(g)),tauross[g],np.linspace(0,1,ntau),kind='linear')
+
+    
+    # Let's interpolate for every depth
+    points = (np.arange(2),np.arange(2),np.arange(2))
+    for i in np.arange(ntau-1)+1:
+        for j in range(ncols):
+            grid[0,0,0,j] = dln.interp(tauross1[1:],model1[j,1:],tauross_new[i],kind='linear')
+            grid[0,0,1,j] = dln.interp(tauross2[1:],model2[j,1:],tauross_new[i],kind='linear')
+            grid[0,1,0,j] = dln.interp(tauross3[1:],model3[j,1:],tauross_new[i],kind='linear')
+            grid[0,1,1,j] = dln.interp(tauross4[1:],model4[j,1:],tauross_new[i],kind='linear')
+            grid[1,0,0,j] = dln.interp(tauross5[1:],model5[j,1:],tauross_new[i],kind='linear')
+            grid[1,0,1,j] = dln.interp(tauross6[1:],model6[j,1:],tauross_new[i],kind='linear')
+            grid[1,1,0,j] = dln.interp(tauross7[1:],model7[j,1:],tauross_new[i],kind='linear')
+            grid[1,1,1,j] = dln.interp(tauross8[1:],model8[j,1:],tauross_new[i],kind='linear')
+            model[j,i] = interpn(points,grid[:,:,:,j],(mapteff,maplogg,mapmetal),method='linear')
+
+    for j in range(ncols):
+        model[j,0] = model[j,1]*0.999
+
+    import pdb; pdb.set_trace()
+        
+
+    # Editing the header
+    tmpstr = header[0]
+    strput,tmpstr,string(teff,format='(f7.0)'),5
+    strput,tmpstr,string(logg,format='(f8.5)'),21
+    header[0] = tmpstr
+    tmpstr1 = header[1]
+    tmpstr2 = header[4]
+    if (metal < 0.0):
+        if type == 'old':
+            strput,tmpstr1,string(abs(metal),format='("-",f3.1)'),18
+        else:
+            strput,tmpstr1,string(abs(metal),format='("-",f3.1)'),8
+        strput,tmpstr2,string(10^metal,format='(f9.5)'),16
+    else:
+        if type == 'old':
+            strput,tmpstr1,string(abs(metal),format='("+",f3.1)'),18
+        else:
+            strput,tmpstr1,string(abs(metal),format='("+",f3.1)'),8
+        strput,tmpstr2,string(10^metal,format='(f9.5)'),16
+    header[1] = tmpstr1
+    header[4] = tmpstr2
+    tmpstr = header[22]
+    strput,tmpstr,string(ntau,format='(i2)'),11
+    header[22] = tmpstr
 
     return model, header, tail
 
