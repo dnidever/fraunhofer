98aae59c4b96c79fa8fcf94af55c035268ded95a
David Nidever <dnidever@lsst.org>
2023-07-21 15:40:52 -0600

    bumped version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Fraunhofer',
-      version='1.0.0',
+      version='1.0.1',
       description='Generic Steller Abundance Determination Software',
       author='David Nidever',
       author_email='dnidever@montana.edu',
